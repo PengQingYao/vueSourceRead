056028729b113074612045d9e800e3359d697214
PengQingYao <791812666@qq.com>
2019-06-09 13:07:35 +0800

    test

---
+++ src/core/observer/dep.js
@@ -0,0 +1,66 @@
+/* @flow */
+
+import type Watcher from './watcher'
+import { remove } from '../util/index'
+import config from '../config'
+
+let uid = 0
+
+/**
+ * A dep is an observable that can have multiple
+ * directives subscribing to it.
+ */
+export default class Dep { //用来保存收集的依赖，
+  static target: ?Watcher; //target是一个watch的实例
+  id: number;
+  subs: Array<Watcher>; //缓存target的实例（可以有多个），一个数组。
+
+  constructor () {
+    this.id = uid++
+    this.subs = []
+  }
+
+  addSub (sub: Watcher) {
+    this.subs.push(sub)
+  }
+
+  removeSub (sub: Watcher) {
+    remove(this.subs, sub)
+  }
+
+  depend () {
+    if (Dep.target) {
+      addDep(this) //deps数组中增加自己
+    }
+  }
+
+  notify () { //刷新dom。
+    // stabilize the subscriber list first
+    const subs = this.subs.slice()
+    if (process.env.NODE_ENV !== 'production' && !config.async) {
+      // subs aren't sorted in scheduler if not running async
+      // we need to sort them now to make sure they fire in correct
+      // order
+      subs.sort((a, b) => a.id - b.id)
+    }
+    for (let i  =0, l = subs.length; i < l; i++) {
+      subs[i].update()
+    }
+  }
+}
+
+// The current target watcher being evaluated.
+// This is globally unique because only one watcher
+// can be evaluated at a time.
+Dep.target = null
+const targetStack = []
+
+export function pushTarget (target: ?Watcher) {
+  targetStack.push(target)
+  Dep.target = target
+}
+
+export function popTarget () { //删除最后一个数组
+  targetStack.pop()
+  Dep.target = targetStack[targetStack.length - 1]
+}
